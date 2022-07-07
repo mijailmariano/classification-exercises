66341f9056bf01915d9426d35450d86cfba96a4f
Mijail Q Mariano <mijail.mariano@gmail.com>
2022-07-07 18:18:39 -0400

    071800JUL22 updates

---
+++ prepare.py
@@ -0,0 +1,53 @@
+from pandas import DataFrame
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+def handle_missing_values(df):
+    return df.assign(
+        embark_town=df.embark_town.fillna('Other'),
+        embarked=df.embarked.fillna('O'),
+    )
+
+def remove_columns(df):
+    return df.drop(columns=['deck'])
+
+def encode_embarked(df):
+    encoder = LabelEncoder()
+    encoder.fit(df.embarked)
+    return df.assign(embarked_encode = encoder.transform(df.embarked))
+
+def prep_titanic_data(df):
+    df = df\
+        .pipe(handle_missing_values)\
+        .pipe(remove_columns)\
+        .pipe(encode_embarked)
+    return df
+
+def train_validate_test_split(df, seed=123):
+    train_and_validate, test = train_test_split(
+        df, test_size=0.2, random_state=seed, stratify=df.survived
+    )
+    train, validate = train_test_split(
+        train_and_validate,
+        test_size=0.3,
+        random_state=seed,
+        stratify=train_and_validate.survived,
+    )
+    return train, validate, test
+
+
+
+def split(df, stratify_by=None):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.2, random_state=123)
+        train, validate = train_test_split(train, test_size=.3, random_state=123)
+    else:
+        train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(train, test_size=.3, random_state=123, stratify=train[stratify_by])
+    
+    return train, validate, test
